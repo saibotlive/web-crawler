7c93d26c620004dabc6c39013e9b2adf4a0a4b4e
Tobias Ighofose <tobias.ighofose@turner.com>
2019-02-18 10:24:37 +0000

    feat(web-crawler): add url resolve

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
 import React, { Fragment, useState } from 'react';
 import cheerio from 'cheerio';
+import urlParse from 'url';
 import { GlobalStyle, Text, Form, Button, Status } from './App.styles';
 
 const App = () => {
-  const [status, setStatus] = useState('');
-  const [urlText, setValue] = useState('');
-  const corsAnywhere = 'https://saibot-no-cors.herokuapp.com/';
+    const [status, setStatus] = useState('');
+    const [urlText, setValue] = useState('');
+    const corsAnywhere = 'https://saibot-no-cors.herokuapp.com/';
 
-  const download = (filename, data) => {
-    var element = document.createElement('a');
-    element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(data)}`);
-    element.setAttribute('download', filename);
+    const download = (filename, data) => {
+        var element = document.createElement('a');
+        element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(data)}`);
+        element.setAttribute('download', filename);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+        element.style.display = 'none';
+        document.body.appendChild(element);
 
-    element.click();
+        element.click();
 
-    document.body.removeChild(element);
-  };
+        document.body.removeChild(element);
+    };
 
-  const getAllLinks = $ => {
-    const linksObj = $('a[href], img[src]');    
-    const links = Object.values(linksObj)
-      .filter(val => val.attribs)
-      .map(link => link.attribs.href || link.attribs.src);
+    const getAllLinks = ($) => {
+        const linksObj = $('a[href], img[src]');
+        const links = Object.values(linksObj)
+            .filter((val) => val.attribs)
+            .map((link) => urlParse.resolve(urlText, link.attribs.href || link.attribs.src));
+        if (links.length > 0) {
+            setStatus('Downloaded');
+            download('sitemap.json', JSON.stringify(links));
+        } else setStatus('Nothing to crawl');
+    };
 
-    if (links.length > 0) {
-      setStatus('Downloaded');
-      download('sitemap.json', JSON.stringify(links));
-    } else setStatus('Nothing to crawl');
-  };
+    const crawlURL = async (url) => {
+        setStatus('Crawling...');
+        try {
+            const res = await fetch(`${corsAnywhere}${url}`);
+            const body = await res.text();
+            const $ = cheerio.load(body);
 
-  const crawlURL = async url => {
-    setStatus('Crawling...');
-    try {
-      const res = await fetch(`${corsAnywhere}${url}`);
-      const body = await res.text();
-      const $ = cheerio.load(body);
+            getAllLinks($);
+        } catch (error) {
+            setStatus(error.message);
+        }
+    };
 
-      getAllLinks($);
-    } catch (error) {
-      setStatus(error.message);
-    }
-  };
+    const addHttp = (url) => {
+        if (!/^(?:f|ht)tps?:\/\//.test(url)) {
+            url = 'http://' + url;
+        }
+        return url;
+    };
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
+    const handleChange = (e) => {
+        setValue(addHttp(e.target.value));
+    };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    crawlURL(urlText);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        crawlURL(urlText);
+    };
 
-  return (
-    <Fragment>
-      <GlobalStyle />
-      <Form onSubmit={handleSubmit}>
-        <Text onChange={handleChange} />
-        <Button>Submit</Button>
-        <Status>{status}</Status>
-      </Form>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <GlobalStyle />
+            <Form onSubmit={handleSubmit}>
+                <Text onChange={handleChange} placeholder="http://" />
+                <Button>Submit</Button>
+                <Status>{status}</Status>
+            </Form>
+        </Fragment>
+    );
 };
 
 export default App;
